ce6ab2b8731ea242246053f31be6c0cdc749a5fc
Matthice Storms <matthice.storms@hotmail.com>
2025-05-15 17:23:45 +0200

    trying tot connect backend to frontend, pls kill me

--- flare-app/src/app/page.tsx
+++ flare-app/src/app/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-
 import Link from "next/link"; // zorgt ervoor dat alleen content area reload wordt bij verandering van pagina
 //next js components zijn server side by default
 import ProductCard from "./components/ProductCard";
-import { useEffect, useState } from "react";
-import { getFlares } from "../lib/api";
+import { get } from "./server/user";
 
 type Flare = {
   id: number;
@@ -15,50 +12,13 @@ type Flare = {
   created_at: string;
 };
 
-export default function Home() {
-  const [flares, setFlares] = useState<Flare[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getFlares();
-        setFlares(data);
-      } catch (err) {
-        setError("Failed to load flares");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (loading) return <p className="p-4">Loading flares...</p>;
-  if (error) return <p className="p-4 text-red-500">{error}</p>;
-
+export default async function Home() {
+  const data = await get();
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Nearby Flares</h1>
-      <ul className="space-y-4">
-        {flares.map((flare) => (
-          <li key={flare.id} className="border p-4 rounded-md shadow-md">
-            <p>
-              <strong>Note:</strong> {flare.note}
-            </p>
-            <p>
-              <strong>Category:</strong> {flare.category}
-            </p>
-            <p>
-              <strong>Location:</strong> {flare.latitude}, {flare.longitude}
-            </p>
-            <p className="text-sm text-gray-500">
-              Posted: {new Date(flare.created_at).toLocaleString()}
-            </p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      {data.map((user) => (
+        <p key={user.id}>{user.email}</p>
+      ))}
     </div>
   );
 }
