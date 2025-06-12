473e19159090470cbbc031a9ed963756f4fb09bd
Matthice Storms <matthice.storms@hotmail.com>
2025-06-12 16:36:27 +0200

    authµ

--- flare-app/src/lib/axios.ts
+++ flare-app/src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { KnownPlace } from "@/types/knownPlace";
 import { Flare } from "@/types/flare";
 import { User } from "@/types/user";
+import Cookies from "js-cookie";
 
 // Web routes (no /api prefix)
 const webApi = axios.create({
@@ -21,6 +22,14 @@ const api = axios.create({
     "Content-Type": "application/json",
     Accept: "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  const xsrfToken = Cookies.get("XSRF-TOKEN");
+  if (xsrfToken) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(xsrfToken);
+  }
+  return config;
 });
 
 type LoginCredentials = {
