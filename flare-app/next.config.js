a8fd1cd5059f7cf65def62bb075de244873df0e9
Matthice Storms <matthice.storms@hotmail.com>
2025-06-16 21:53:38 +0200

    images

--- flare-app/next.config.js
+++ flare-app/next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "flare-app-q3qkb.ondigitalocean.app", // production
-        pathname: "/storage/**",
+        pathname: "workspace/storage/**",
       },
     ],
   },
